fa7a36742eb5f2d6c040292ff30ab4eb6f41b3ae
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2023-12-03 11:49:35 +0100

    balazs-matyassy: updates

--- schema.sql
+++ schema.sql
@@ -1,17 +1,19 @@
 DROP TABLE IF EXISTS "recipe";
 DROP TABLE IF EXISTS "user";
 
-CREATE TABLE "user" (
-  "id"              INTEGER PRIMARY KEY AUTOINCREMENT,
-  "username"        TEXT UNIQUE NOT NULL,
-  "password"        TEXT NOT NULL,
-  "role"            TEXT NOT NULL
+CREATE TABLE "user"
+(
+    "id"       INTEGER PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT UNIQUE NOT NULL,
+    "password" TEXT        NOT NULL,
+    "role"     TEXT        NOT NULL
 );
 
-CREATE TABLE "recipe" (
-  "id"              INTEGER PRIMARY KEY AUTOINCREMENT,
-  "category"        TEXT NOT NULL,
-  "name"            TEXT NOT NULL,
-  "description"     TEXT NOT NULL,
-  "difficulty"      INTEGER NOT NULL
+CREATE TABLE "recipe"
+(
+    "id"          INTEGER PRIMARY KEY AUTOINCREMENT,
+    "category"    TEXT    NOT NULL,
+    "name"        TEXT    NOT NULL,
+    "description" TEXT    NOT NULL,
+    "difficulty"  INTEGER NOT NULL
 );
